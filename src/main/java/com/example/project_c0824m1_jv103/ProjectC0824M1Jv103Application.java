8e80e3da6667dbde0657f373d1e92787d8681e07
vantai0410 <vantai04102004@gmail.com>
2025-05-27 19:46:38 +0700

    create project 27/05/2025

---
+++ src/main/java/com/example/project_c0824m1_jv103/ProjectC0824M1Jv103Application.java
@@ -0,0 +1,13 @@
+package com.example.project_c0824m1_jv103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectC0824M1Jv103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectC0824M1Jv103Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_c0824m1_jv103/ProjectC0824M1Jv103ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_c0824m1_jv103;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectC0824M1Jv103ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
