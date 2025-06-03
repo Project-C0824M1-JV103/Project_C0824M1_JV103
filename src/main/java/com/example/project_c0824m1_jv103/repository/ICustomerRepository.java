f651539c6cece7b2a438d10ab99a8a09446d4ca1
Viết Tài <trantafi204@gmail.com>
2025-06-03 19:00:48 +0700

    add

--- src/main/java/com/example/project_c0824m1_jv103/repository/ICustomerRepository.java
+++ src/main/java/com/example/project_c0824m1_jv103/repository/ICustomerRepository.java
@@ -2,6 +2,14 @@ package com.example.project_c0824m1_jv103.repository;
 
 import com.example.project_c0824m1_jv103.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface ICustomerRepository extends JpaRepository<Customer, Integer> {
+
+
 }
