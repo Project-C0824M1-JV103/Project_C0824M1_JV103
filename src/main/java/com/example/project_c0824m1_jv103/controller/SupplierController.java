4890c0880abc887ecbba174569a92ee8f751cb43
Le Tran Cong Dat <lephuonguyen021@gmail.com>
2025-06-14 18:07:06 +0700

    list nhà cung cấp

---
+++ src/main/java/com/example/project_c0824m1_jv103/controller/SupplierController.java
@@ -0,0 +1,20 @@
+package com.example.project_c0824m1_jv103.controller;
+
+import com.example.project_c0824m1_jv103.service.supplier.ISupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/Supplier")
+public class SupplierController {
+    @Autowired
+    private ISupplierService supplierService;
+
+    @GetMapping("")
+    public ModelAndView showSupplierList() {
+        return new ModelAndView("supplier/list-supplier").addObject("suppliers", supplierService.findAll());
+    }
+}
