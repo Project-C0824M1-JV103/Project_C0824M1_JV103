4437c12082a77fdaafc5ff323b876218a1227fb3
vantai0410 <vantai04102004@gmail.com>
2025-06-24 19:18:44 +0700

    sale history

--- src/main/java/com/example/project_c0824m1_jv103/service/sale/SaleService.java
+++ src/main/java/com/example/project_c0824m1_jv103/service/sale/SaleService.java
@@ -6,6 +6,8 @@ import com.example.project_c0824m1_jv103.model.Product;
 import com.example.project_c0824m1_jv103.repository.ISaleRepository;
 import com.example.project_c0824m1_jv103.repository.IProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -41,5 +43,10 @@ public class SaleService implements ISaleService {
     @Override
     public Optional<Sale> findById(Integer id) {
         return saleRepository.findById(id);
+    }
+
+    @Override
+    public Page<Sale> findAll(Pageable pageable) {
+        return saleRepository.findAll(pageable);
     }
 }
