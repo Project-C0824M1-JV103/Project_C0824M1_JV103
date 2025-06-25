2e0c73d6e1653744630ec1626d44563c65b22c6d
Minh Phuc <caoquangminhphuc@gmail.com>
2025-06-25 17:05:46 +0700

    fix edit employeedto

--- src/main/java/com/example/project_c0824m1_jv103/dto/EmployeeEditDto.java
+++ src/main/java/com/example/project_c0824m1_jv103/dto/EmployeeEditDto.java
@@ -8,17 +8,18 @@ import jakarta.validation.constraints.Size;
 public class EmployeeEditDto {
     private Integer employeeId;
 
-    @NotBlank(message = "Nhập tên đầy đủ!")
+    @NotBlank(message = "Vui lòng nhập tên đầy đủ!")
     @Size(max = 50, message = "Tên không được vượt quá 50 ký tự")
+    @Pattern(regexp = "^[\\p{L}\\s]+$", message = "Tên không được chứa số hoặc kí tự đặc biệt!")
     private String fullName;
 
-    @NotBlank(message = "Nhập email đầy đủ!")
-    @Email(message = "Email không hợp lệ")
+    @NotBlank(message = "Vui lòng nhập email đầy đủ!")
     @Size(max = 50, message = "Email không được vượt quá 50 ký tự")
-    //patern
+//    @Email(message = "Vui lòng nhập email đúng định dạng!")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "Vui lòng nhập email đúng định dạng!")
     private String email;
 
-    @Pattern(regexp = "^[0-9]{10,11}$", message = "Số điện thoại phải có 10 hoặc 11 chữ số!")
+    @Pattern(regexp = "^[0-9]{10,11}$", message = "Vui lòng nhập số điện thoại với 10 hoặc 11 chữ số!")
     private String phone;
 
     @NotBlank(message = "Chọn vị trí")
